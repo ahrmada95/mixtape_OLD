24a6fcf82cca492d0ee70c131eb22d158ee561c9
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-08-02 10:14:31 -0400

    logic for checking complete

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,7 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './login.css'
 
 const LoginForm = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect ( () => {
+        const fetchData = async () => {
+            const req = await fetch('http://localhost:4001/user/credentials/all');
+            const res = await req.json();
+            setUsers(res);
+        }
+        fetchData();
+    }, [])
+
+    //verification function that is case sensitive
+    const checkUser = (username, password) => {
+        for (let a=0; a<users.length; a++){
+            if(users[a]['username'] === username && users[a]['password'] === password){
+                console.log('true');
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
     return (
         <div id="login-form">
             <form>
@@ -23,7 +50,7 @@ const LoginForm = () => {
                                                 <br/>
                                                 <br/>
                 <input type="checkbox" id="remember-login" name="remember-login" value="Bike"/>
-                <label for="vehicle1" id="remember-login"> Remember Me</label>
+                <label htmlFor="vehicle1" id="remember-login"> Remember Me</label>
                                                 <br/>
                 <button type="submit" className="login-btn">Login</button>
             </form>
